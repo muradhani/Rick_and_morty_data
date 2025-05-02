7cdeea2be53467bf89a1c7664f463e1a865456f8
muradhani <mouradhany574@gmail.com>
2025-05-02 20:19:05 +0300

    Add EpisodesRepository and EpisodesRepositoryImpl

---
+++ src/main/java/com/example/data/repositories/EpisodesRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.data.repositories
+
+import com.example.data.mappers.EpisodeMapper
+import com.example.domain.interfaces.EpisodesRepository
+import com.example.domain.modules.Episode
+import com.example.network.KtorClient
+import javax.inject.Inject
+
+class EpisodesRepositoryImpl @Inject constructor(
+    private val mapper: EpisodeMapper,
+    private val ktorClient: KtorClient
+) : EpisodesRepository {
+    override suspend fun getEpisodes(episodeIds: String): List<Episode> {
+        val episodes = ktorClient.getEpisodes(episodeIds)
+       return episodes?.map { mapper.dtoToEntity(it) } ?: emptyList()
+    }
+}
