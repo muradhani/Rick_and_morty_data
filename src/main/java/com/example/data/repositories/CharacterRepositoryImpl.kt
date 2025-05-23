a8d2e59c0c9b080eb17108b79a81dc67ad156ce7
muradhani <mouradhany574@gmail.com>
2025-05-23 21:06:07 +0300

    Integrate local caching for characters
    This commit introduces local caching for character data using Room:
    
    - When fetching characters, if it's the first page, the local cache is cleared.
    - Fetched characters are then inserted into the local database.
    - When retrieving a specific character by ID, the local cache is checked first.

--- src/main/java/com/example/data/repositories/CharacterRepositoryImpl.kt
+++ src/main/java/com/example/data/repositories/CharacterRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.example.data.repositories
 
+import com.example.data.database.Dao.CharacterDao
 import com.example.data.mappers.domainMapper.CharacterMapper
 import com.example.data.mappers.domainMapper.CharactersPageMapper
+import com.example.data.mappers.networkMapper.CharacterNetworkMapper
+import com.example.data.mappers.networkMapper.characterDaoMapper
 import com.example.domain.interfaces.CharacterRepository
 import com.example.domain.modules.Character
 import com.example.domain.modules.CharactersPage
@@ -12,10 +15,16 @@ import javax.inject.Inject
 class CharacterRepositoryImpl @Inject internal constructor(
     private val characterMapper : CharacterMapper,
     private val ktorClient: KtorClient,
-    private val charactersPageMapper: CharactersPageMapper
+    private val charactersPageMapper: CharactersPageMapper,
+    private val characterDao : CharacterDao,
+    private val characterNetworkMapper: CharacterNetworkMapper,
+    private val characterDaoMapper: characterDaoMapper
 ) : CharacterRepository {
     override suspend fun getCharacter(id: Int): Character? {
         var character : Character? = null
+        characterDao.getCharacter(id).let {
+            character = characterDaoMapper.dtoToEntity(it)
+        }
         ktorClient.getCharacter(id)?.let {
             character = characterMapper.dtoToEntity(it)
         }
@@ -23,7 +32,10 @@ class CharacterRepositoryImpl @Inject internal constructor(
     }
 
     override suspend fun getCharactersPaging(pageNumber: Int): CharactersPage? {
-        return ktorClient.getCharactersPage(pageNumber)?.let {
+        if (pageNumber == 1) characterDao.deleteAllCharacter()
+        return ktorClient.getCharactersPage(pageNumber)?.also {
+            characterDao.insertAllCharacter(characterNetworkMapper.dtoListToDaoList(it.results))
+        }?.let {
             charactersPageMapper.dtoToEntity(it)
         }
     }
