0c8eaff4457916e1759f0054fc9ccfda3cc352e9
muradhani <mouradhany574@gmail.com>
2025-05-23 20:58:03 +0300

    Add Character ID to CharacterEntity

--- src/main/java/com/example/data/mappers/networkMapper/CharacterNetworkMapper.kt
+++ src/main/java/com/example/data/mappers/networkMapper/CharacterNetworkMapper.kt
@@ -3,10 +3,12 @@ package com.example.data.mappers.networkMapper
 import com.example.data.database.entities.CharacterEntity
 import com.example.data.mappersInterfaces.Mapper
 import com.example.network.dto.CharacterDto
+import javax.inject.Inject
 
-class CharacterNetworkMapper : Mapper<CharacterDto, CharacterEntity> {
+class CharacterNetworkMapper @Inject constructor() : Mapper<CharacterDto, CharacterEntity> {
     override fun dtoToDao(input: CharacterDto): CharacterEntity {
         return CharacterEntity(
+            characterId = input.id,
             character = input
         )
     }
