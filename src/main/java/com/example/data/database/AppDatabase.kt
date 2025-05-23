52153ca8f4e8046e653c9a265183aea2eebe72ce
muradhani <mouradhany574@gmail.com>
2025-05-23 18:51:10 +0300

    Add Gson TypeConverter for CharacterEntity and CharacterDto

--- src/main/java/com/example/data/database/AppDatabase.kt
+++ src/main/java/com/example/data/database/AppDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.data.database.Dao.CharacterDao
+import com.example.data.database.converter.Converters
 import com.example.data.database.entities.CharacterEntity
 
-@Database(entities = [CharacterEntity::class], version = 1)
+@Database(entities = [CharacterEntity::class], version = 2)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun characterDao(): CharacterDao
 }
---
+++ src/main/java/com/example/data/database/converter/Converters.kt
@@ -0,0 +1,29 @@
+package com.example.data.database.converter
+
+import androidx.room.TypeConverter
+import com.example.data.database.entities.CharacterEntity
+import com.example.network.dto.CharacterDto
+import com.google.gson.Gson
+
+class Converters {
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromCharacterEntity(character: CharacterEntity): String {
+        return gson.toJson(character)
+    }
+
+    @TypeConverter
+    fun fromCharacterEntity(json: String): CharacterEntity {
+        return gson.fromJson(json, CharacterEntity::class.java)
+    }
+    @TypeConverter
+    fun fromCharacterDto(character: CharacterDto): String {
+        return gson.toJson(character)
+    }
+
+    @TypeConverter
+    fun fromCharacterDto(json: String): CharacterDto {
+        return gson.fromJson(json, CharacterDto::class.java)
+    }
+}
